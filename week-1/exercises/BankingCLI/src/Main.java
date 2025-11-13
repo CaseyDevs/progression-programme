3075b9c3f42dd1f2f0baa21a2375e63d52765a8b
CaseyDevs <CaseyDevs5@gmail.com>
2025-11-13 20:34:40 +0000

    feat: get user choice & validate input

--- week-1/exercises/BankingCLI/src/Main.java
+++ week-1/exercises/BankingCLI/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
 
         String[] options = {"Deposit", "Check Balance", "Withdraw"};
         BankAccount account1 = new BankAccount(1000);
@@ -10,15 +14,25 @@ public class Main {
                 "##############################\n"
         );
 
-        System.out.println("Choose an option: ");
-
+        // Output options
+        System.out.println("Options:");
         for(int i = 0; i < options.length; i++) {
             System.out.println(i+1 + ": " + options[i]);
         }
 
-        // TODO:
-        //  ADD WITHDRAW METHOD,
-        //  CLI MENU USING SCANNER,
-        //  IMPROVE INPUT VALIDATION
+        // Gather and validate user input
+        while (!validInput) {
+            System.out.println("Please enter an option number: ");
+            if (scanner.hasNextInt()) { // Check is input value is of type int
+                int userChoice = scanner.nextInt();
+                if (userChoice >= 1 && userChoice <= options.length) { // Ensure input is a valid option
+                    validInput = true;
+                } else {
+                    System.out.println("Invalid input! Please enter one of the option numbers.");
+                    scanner.next(); // Clear the invalid input
+                }
+            }
+        }
+
     }
 }
