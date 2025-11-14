32f52a13b891caa438cbf5baab75efabac761eac
CaseyDevs <CaseyDevs5@gmail.com>
2025-11-14 12:01:04 +0000

    feat: create transaction history and related methods

--- week-1/exercises/BankingCLI/src/BankAccount.java
+++ week-1/exercises/BankingCLI/src/BankAccount.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankAccount {
     private double balance;
+    private List<String> transactionHistory;
+    private String TRANSACTION_TYPE;
 
     public BankAccount(double balance) {
         if (balance < 0) {
             throw new IllegalArgumentException("Initial balance cannot be negative");
         }
         this.balance = balance;
+        this.transactionHistory = new ArrayList<String>();
     }
 
     public double getBalance() {
@@ -13,10 +19,16 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
+        TRANSACTION_TYPE = "DEPOSIT";
+        String transactionItem;
+
         if (amount <= 0) {
             throw new IllegalArgumentException("Deposit amount must be positive");
         }
         balance += amount;
+        transactionItem = TRANSACTION_TYPE + ": " + amount;
+
+        addToTransactionHistory(transactionItem);
     }
 
     public boolean withdraw(double amount) {
@@ -28,6 +40,16 @@ public class BankAccount {
         } else {
             balance -= amount;
             return true;
+        }
+    }
+
+    public void addToTransactionHistory(String transactionItem) {
+        transactionHistory.add(transactionHistory.size(), transactionItem);
+    }
+
+    public void getTransactionHistory() {
+        for(String s : transactionHistory) {
+            System.out.println(s);
         }
     }
 }
