d6ecba69cdf1a22c5dda6ba4fc607f41e745ffd1
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-02 01:29:16 +0000

    refactor: move account service logic into AccountService

---
+++ main/projects/banking-api/src/main/java/com/casey/bankingapi/service/AccountService.java
@@ -0,0 +1,26 @@
+package com.casey.bankingapi.service;
+
+import com.casey.bankingapi.dto.AccountDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+class AccountService {
+    private final List<String> accounts = new ArrayList<>();
+
+
+    public void createAccount(String name) {
+        if (!accounts.contains(name)) {
+            accounts.add(name);
+        }
+    }
+
+    public List<AccountDto> getAllAccounts() {
+        return accounts.stream()
+                .map(AccountDto::new)
+                .collect(Collectors.toList());
+    }
+}
--- main/projects/banking-api/src/main/java/com/casey/bankingapi/service/BankingService.java
+++ main/projects/banking-api/src/main/java/com/casey/bankingapi/service/BankingService.java
@@ -9,22 +9,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class BankingService {
-    private final List<String> accounts = new ArrayList<>();
 
     public String getWelcomeMessage(String name) {
         return "Welcome " + name + "!";
-    }
-
-    public void createAccount(String name) {
-        if (!accounts.contains(name)) {
-            accounts.add(name);
-        }
-        // Could throw exception if account already exists
-    }
-
-    public List<AccountDto> getAllAccounts() {
-        return accounts.stream()
-                .map(AccountDto::new)
-                .collect(Collectors.toList());
     }
 }
