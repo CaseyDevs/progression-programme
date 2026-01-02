ea83c83edf49b9ef11970c2f9a867612fbebdc2a
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-02 13:01:34 +0000

    refactor: map account list to dtos

--- main/projects/banking-api/src/main/java/com/casey/bankingapi/domain/Account.java
+++ main/projects/banking-api/src/main/java/com/casey/bankingapi/domain/Account.java
@@ -3,11 +3,11 @@ package com.casey.bankingapi.domain;
 import java.math.BigDecimal;
 
 public class Account {
-    String accountName;
-    String accountType;
-    BigDecimal balance;
+    public String accountName;
+    public String accountType;
+    public BigDecimal balance;
 
-    Account(String accountName, String accountType, BigDecimal balance) {
+    public Account(String accountName, String accountType, BigDecimal balance) {
         this.accountName = accountName;
         this.accountType = accountType;
         this.balance = balance;
--- main/projects/banking-api/src/main/java/com/casey/bankingapi/service/AccountService.java
+++ main/projects/banking-api/src/main/java/com/casey/bankingapi/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.casey.bankingapi.service;
 
+import com.casey.bankingapi.domain.Account;
 import com.casey.bankingapi.dto.AccountResponseDto;
 import com.casey.bankingapi.exceptions.AccountNotFoundException;
 import com.casey.bankingapi.repository.AccountRepository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
@@ -19,11 +21,24 @@ public class AccountService {
 
 
     public void createAccount(String accountName, String accountType, BigDecimal balance) {
-        repo.addAccount(new AccountResponseDto(accountName, accountType, balance));
+        repo.addAccount(new Account(accountName, accountType, balance));
     }
 
 
     public List<AccountResponseDto> getAllAccounts() throws AccountNotFoundException {
-        return repo.getAccounts();
+        List<Account> accounts = repo.getAccounts();
+
+        if (!accounts.isEmpty()) {
+            // Map accounts to dto
+            return accounts.stream()
+                    .map(account -> new AccountResponseDto(
+                            account.accountName,
+                            account.accountType,
+                            account.balance
+                    ))
+                    .collect(Collectors.toList());
+        } else {
+            throw new AccountNotFoundException();
+        }
     }
 }
